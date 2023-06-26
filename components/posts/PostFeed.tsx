bd96da040af27ee32d49e8a89e7f8328adecec41
lofi-sea <71112296+wannabe2266@users.noreply.github.com>
2023-06-26 20:37:32 +0700

    load and create post

---
+++ components/posts/PostFeed.tsx
@@ -0,0 +1,29 @@
+import usePosts from '@/hooks/usePosts';
+import React from 'react'
+import PostItem from './PostItem';
+
+interface PostFeedProps {
+    userId?: string;
+}
+
+const PostFeed:React.FC<PostFeedProps> = ({userId}) => {
+
+    const { data: posts = [] } = usePosts(userId);
+
+  return (
+    <>
+        {
+            posts.map((post: Record<string,any>) => (
+                <PostItem
+                userId={userId}
+                key={posts.id}
+                data={post}
+                />
+
+            ))
+        }
+    </>
+  )
+}
+
+export default PostFeed
